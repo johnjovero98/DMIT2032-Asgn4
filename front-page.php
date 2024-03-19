2c57d72670ec8a18e27590e0b39a8d1e297e368b
johnjovero98 <john.jovero@outlook.com>
2024-03-19 10:26:49 -0600

    intial commit

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The front-page template file.
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ *
+ * @package HML Classic
+ */
+get_header();
+?> 
+<h1>HML Construction Landing Page</h1>
+<?php get_footer(); ?>
