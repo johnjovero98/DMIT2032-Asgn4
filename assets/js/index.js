6d4ff5f40c75297e654dc9b340588af2a76372c8
johnjovero98 <john.jovero@outlook.com>
2024-03-22 22:20:37 -0600

    add chevron to nav  links with children

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,21 @@
 const menuButton = document.querySelector('.menu-bttn');
-let menuItem = document.querySelectorAll('.menu-item')
+let menuItem = document.querySelectorAll('.menu-item');
 
+// add click event for menu items in mobile view
 console.log(menuItem)
-
-
 menuButton.addEventListener("click", function(){
     const menuNav = document.querySelector('nav')
     menuNav.classList.toggle('show-nav')
 })
+
+
+// Add chevron to nav list items with sub menu
+const chevronSVGHTML = `<svg  height="15px" width="15px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                    viewBox="0 0 407.437 407.437" xml:space="preserve"> <polygon points="386.258,91.567 203.718,273.512 21.179,91.567 0,112.815 203.718,315.87 407.437,112.815 "/>
+                    </svg>`
+for (i = 0; i < menuItem.length; i++) {
+    if (menuItem[i].classList.contains('menu-item-has-children')) {
+        let menuLink = menuItem[i].childNodes[0]    
+        menuLink.insertAdjacentHTML('beforeend',chevronSVGHTML)
+    }
+}
