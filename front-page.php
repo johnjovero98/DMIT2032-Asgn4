dc3f884ef954c8b009ff786dae873b93595e5334
johnjovero98 <john.jovero@outlook.com>
2024-03-28 20:38:58 -0600

    code refactored

--- front-page.php
+++ front-page.php
@@ -5,15 +5,6 @@
         <h2>Where Design Comes to Life</h2>
         <p>We are a dedicated St. Albert and Edmonton landscaping design and construction
             team who value the beauty that great design and workmanship can bring to a space.</p>
-
-        <!-- wp:buttons -->
-        <div class=" contact-us wp-block-buttons"><!-- wp:button -->
-            <div class="wp-block-button">
-                <a class="wp-block-button__link wp-element-button" href="https://jjovero1.dmitstudent.ca/dmit2032/classic-site/contact-us/">Get Started</a>
-            </div>
-            <!-- /wp:button -->
-        </div>
-        <!-- /wp:buttons -->
     </div>
 
     <div class="overlay"></div>
